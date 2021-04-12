44a52fdbf58b636ffc2bcdce2e756ee86fce7aae
Viktor Arlashyn <RandomNotTakenName@users.noreply.github.com>
2021-04-12 12:42:48 +0300

    slider begun

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
 
-var number=1, position, anchor;
+var i, imageArray=["photo/123.jpg","photo/20200920_143758.jpg","photo/20201011_110541.jpg","photo/IMG_20191009_160452_066.jpg"];
 
-function carouselPlus() {
-    position=number;
-    position++;
-    if (position > 3) {
-        position = 1;
-    }    
-    classChange(position);
-    document.getElementById('CarIm').src = "img" + position + ".jpg";
-    number=position;
-}
-
-function carouselMinus() {
-    position=number;
-    position--;
-    if (position < 1) {
-        position = 3;
-    }
-    classChange(position);
-    document.getElementById('CarIm').src = "img" + position + ".jpg";
-    number=position;
-}
-
-function clickChange(position) {
-    document.getElementById('CarIm').src = "img" + position + ".jpg";
-    classChange(position);
-    number=position;
+function slider(i) {
+    document.getElementById('target').src=imageArray[i];
+    console.log(imageArray[i]);
 }
