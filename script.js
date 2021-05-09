3ef21275d71c00290f4ee243ed9e79d9bcd6a6f2
Viktor Arlashyn <RandomNotTakenName@users.noreply.github.com>
2021-05-09 17:19:31 +0300

    ...

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-var i=0, imageArray=["photo/123.jpg","photo/20200920_143758.jpg","photo/20201011_110541.jpg","photo/IMG_20191009_160452_066.jpg"];
+var i=0, imageArray=["photo/1.jpg","photo/2.jpg","photo/3.jpg","photo/4.jpg"];
 
 function slider() {
     if(i<0) i=3;
