7edd5d56038d4080cd47d8f80002adf97b65c2b0
Viktor Arlashyn <RandomNotTakenName@users.noreply.github.com>
2021-05-15 14:36:46 +0300

    desktop ver 99%

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 
-var i=0, imageArray=["photo/1.jpg","photo/2.jpg","photo/3.jpg","photo/4.jpg"];
+var i, prev=1, imageArray=["photo/1.jpg","photo/2.jpg","photo/3.jpg","photo/4.jpg"];
 
 function slider() {
     if(i<0) i=3;
     if(i>3) i=0;
     document.getElementById('target').src=imageArray[i];
+    document.getElementById('Button' + prev).classList.toggle('active');
+    document.getElementById('Button' + (i + 1)).classList.toggle('active');
+    prev=(i+1);
 }
