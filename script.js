fcf2f7eae03f0b892f17621f50f8bb1f1ee09e48
Viktor Arlashyn <RandomNotTakenName@users.noreply.github.com>
2021-05-16 18:37:02 +0300

    slider js <3

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 
-var i, prev=1, imageArray=["photo/1.jpg","photo/2.jpg","photo/3.jpg","photo/4.jpg"];
+var i = 0, prev = 1, pushed, moving, imageArray = ["photo/1.jpg", "photo/2.jpg", "photo/3.jpg", "photo/4.jpg"];
 
 function slider() {
-    if(i<0) i=3;
-    if(i>3) i=0;
-    document.getElementById('target').src=imageArray[i];
+    if (i < 0) i = 3;
+    if (i > 3) i = 0;
+    document.getElementById('target').src = imageArray[i];
     document.getElementById('Button' + prev).classList.toggle('active');
     document.getElementById('Button' + (i + 1)).classList.toggle('active');
-    prev=(i+1);
+    prev = (i + 1);
+}
+
+function animatron() {
+    if (prev == i) pushed = "rsb"
+    else pushed = "lsb";
+    moving = document.getElementById(pushed);
+    moving.classList.toggle("moving");
+    let scavenger = setTimeout(function() {
+        moving.classList.toggle("moving");
+    }, 500);
+    slider();
 }
