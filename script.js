80f57ef248a4e4fbb4771229f91687574e37c9d4
Viktor Arlashyn <RandomNotTakenName@users.noreply.github.com>
2021-05-27 20:29:13 +0300

    nav dev, small fixes

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 
-var i = 0, prev = 1, pushed, moving, target, imageArray = ["photo/1.jpg", "photo/2.jpg", "photo/3.jpg", "photo/4.jpg"];
+var here = 1, there = 1, i = 0, prev = 1, pushed, moving, target, 
+imageArray = ["photo/1.jpg", "photo/2.jpg", "photo/3.jpg", "photo/4.jpg"],
+someArray = ["0", "800", "1437", "1880", "2482", "3200"],
+navArray = ['nav', 'navGreeting', 'navSkills', 'navExperience', 'navSlider', 'navContacts'];
+
+window.addEventListener("scroll", handle);
 
 function slider() {
     if (i < 0) i = 3;
@@ -25,8 +30,14 @@ function navigator(target) {
     let goTo=document.getElementById(target);
     goTo.scrollIntoView({top: 0, behavior: 'smooth'});
 }
-
-
-
-
-/* element.scrollHeight - element.scrollTop === element.clientHeight */
+function handle() {
+    let seen = (window.innerHeight + window.pageYOffset),
+    here = someArray.findIndex((element) => seen < element);
+    console.log(window.innerHeight, window.pageYOffset, here, there, seen, navArray[here], navArray[there]);
+    if (here != there) {
+    document.getElementById(navArray[here]).classList.toggle('here');
+    document.getElementById(navArray[there]).classList.toggle('here');
+    there = here;
+    }
+    console.log(window.innerHeight, window.pageYOffset, here, there, seen, navArray[here], navArray[there]);
+}
